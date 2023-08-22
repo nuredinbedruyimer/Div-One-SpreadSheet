bcfeb5bd6bc5ce02b0177bea0cac1cc38f472f7e
nuredinbedruyimer <119440292+nuredinbedruyimer@users.noreply.github.com>
2023-08-22 13:57:18 +0300

    Time: 93 ms (87.94%), Space: 14.5 MB (57.82%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,28 +1,25 @@
 class Solution(object):
     def missingNumber(self, nums):
-        n=max(nums)
-        arr=[i  for i in range(n+1)]
-        def is_Exist(target,arr):
-            arr.sort()
-            low=0
-            high=len(arr)-1
-            while low<=high:
-                middle=(low+high)//2
-                if nums[middle]==target:
-                    return True
-                if target > nums[middle]:
-                    low=middle+1
-                else:
-                    high=middle-1
-            return False
-       
-                
-        for num in arr:
-            if is_Exist(num,nums):
-                continue
-            
-            return num 
-        return max(arr)+1
+        '''
+        [3,0,1]
+        [1,2,3]
+        diffrence between Them 
+        [-2,2,2]
+        The Sum Of The Difference become
+        total=-2+2+2
+        total=2
+        as we expect
+        other testcase
+        [0,1]
+        [1,2]
+        [1,1]
+
+        
+        '''
+        target=0
+        for i in range(len(nums)):
+            target+=(i+1-nums[i])
+        return target
             
 
 
