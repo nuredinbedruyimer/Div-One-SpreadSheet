3113611c19fe194148af29fec95bafea831f868d
nuredinbedruyimer <119440292+nuredinbedruyimer@users.noreply.github.com>
2023-08-26 01:32:32 +0300

    Time: 1504 ms (97.85%), Space: 107.2 MB (93.32%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,12 +1,13 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        # Store all edges in 'graph'.
+       
         graph = collections.defaultdict(list)
         for a, b in edges:
             graph[a].append(b)
             graph[b].append(a)
         
-        # Store all the nodes to be visited in 'queue'.
+        
+       
         seen = [False] * n
         seen[source] = True
         queue = collections.deque([source])
@@ -16,8 +17,7 @@ class Solution:
             if curr_node == destination:
                 return True
 
-            # For all the neighbors of the current node, if we haven't visit it before,
-            # add it to 'queue' and mark it as visited.
+         
             for next_node in graph[curr_node]:
                 if not seen[next_node]:
                     seen[next_node] = True
