a0dfe86b39da3ecf21ec18b6e16707f08979a4cd
nuredinbedruyimer <119440292+nuredinbedruyimer@users.noreply.github.com>
2023-10-04 10:29:44 +0300

    Time: 37 ms (61.52%), Space: 16.1 MB (86.81%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        resultStack=[]
+        for index in range(len(s)):
+            if(s[index]!="]"):
+                resultStack.append(s[index])
+            else:
+                substr=""
+                while resultStack[-1]!="[":
+                    substr=resultStack.pop() + substr
+                resultStack.pop()
+                number=""
+                while resultStack and resultStack[-1].isdigit():
+                    number=resultStack.pop()+number
+                resultStack.append(int(number)*substr)
+        result ="".join(resultStack)
+        return result
+                
+                    
+        
