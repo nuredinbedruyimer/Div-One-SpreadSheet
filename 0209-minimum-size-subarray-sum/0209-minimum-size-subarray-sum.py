fef73d029786d5398e6891bbe9b57c2096ac6c24
Nuredin Bedru <119440292+nuredinbedruyimer@users.noreply.github.com>
2023-10-27 12:04:58 +0300

    Time: 214 ms (84.27%), Space: 29.4 MB (64.79%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,14 +1,37 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        left=0
-        tempSum=0
-        ans=float('inf')
-        for right in range(len(nums)):
-            tempSum+=nums[right]
-            while tempSum>=target:
-                ans=min(ans,right-left+1)
-                tempSum-=nums[left]
-                left=left+1
-        return 0 if ans==float('inf') else ans
+        '''2,3,1,2,4,3
+                     L
+                     R
+           Ans=4 4 3 3 2
+           Right Move When Ever There is less Sum Than Target value 
+           Left Move When we Get Larger Or Equal Sum to that of target
+           bacause our target is to find min sub array that equal target
+           make sure you return when you get non
+        
+        '''
+        
+        
+        
+        
+        def my_func(nums,target):
+            left=0
+            right=0
+            ans=float('inf')
+            sum_so_far=0
+            while right<len(nums):
+                sum_so_far+=nums[right]
+                while left<=right and sum_so_far>=target:
+                    sum_so_far-=nums[left]
+                    
+                    ans=min(ans,right-left+1)
+                    left=left+1
+                right=right+1
+            if ans==float('inf'):
+                return 0
+            else:
+                return ans
+        return my_func(nums,target)
                 
-                
+                     
+        
