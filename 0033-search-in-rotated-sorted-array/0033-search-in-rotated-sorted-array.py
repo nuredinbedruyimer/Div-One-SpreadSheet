6dc380e12511f2346290f7da631bbafe522dba77
Nuredin Bedru <119440292+nuredinbedruyimer@users.noreply.github.com>
2023-10-30 23:04:43 +0300

    Time: 44 ms (77.57%), Space: 16.7 MB (45.84%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,20 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left=0
-        right=len(nums)-1
-        while left<=right:
-            middle=(left+right)//2
-            if nums[middle]==target:
-                return middle
-            elif nums[middle]<nums[right]:
-                if nums[middle]<target and nums[right]>=target:
-                    left=middle+1
+        def rotate_one(arr,target):
+            low = 0
+            high = len(arr)-1
+            while low <= high:
+                middle = (low + high)//2
+                if arr[middle] == target:
+                    return middle
+                elif arr[middle] < arr[high]:
+                    if arr[middle] < target <= arr[high]:
+                        low = middle + 1
+                    else:
+                        high = middle-1
                 else:
-                    right=middle-1
-            else:
-                if nums[left]<=target and nums[middle]>target:
-                    right=middle-1
-                else:
-                    left=middle+1
-        return -1
+                    if arr[low] <= target < arr[middle]:
+                        high = middle-1
+                    else:
+                        low = middle + 1
+            return -1
+        return rotate_one(nums,target)
         
